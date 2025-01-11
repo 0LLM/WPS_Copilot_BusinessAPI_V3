5ed3b19121056c1103433a1db30e1ef49e861d68
Kevin-Rose <javaandroidc@qq.com>
2025-01-11 14:40:16 +0800

    update  package name

--- src/main/java/cn/copilot/tool/weboffice/v3/controller/UserController.java
+++ src/main/java/cn/copilot/tool/weboffice/v3/controller/UserController.java
@@ -15,37 +15,40 @@ import java.util.Objects;
 
 /**
  * 用户信息 -> 详见： <br>
- * <a href="https://solution.wps.cn/docs/callback/user.html">wps web office 用户信息</a>
+ * <a href="https://solution.wps.cn/docs/callback/user.html">wps web office
+ * 用户信息</a>
  */
 @RestController
 @RequestMapping("/v3/3rd/users")
 public class UserController extends ProviderBaseController {
 
-    /**
-     * 批量获取用户信息
-     *
-     * @param userIds 用户id列表 <br>
-     *                <a href = "https://solution.wps.cn/docs/callback/user.html#批量获取用户信息">-详见官方文档-</a>
-     */
-    @GetMapping
-    @ProviderJsonApi
-    public ProviderResponseEntity<List<UserInfo>> fetchUsers(@RequestParam("user_ids") List<String> userIds) {
-        return ProviderResponseEntity.ok(this.getServiceOrThrow().fetchUsers(userIds));
-    }
+	/**
+	 * 批量获取用户信息
+	 *
+	 * @param userIds 用户id列表 <br>
+	 *                <a href =
+	 *                "https://solution.wps.cn/docs/callback/user.html#批量获取用户信息">-详见官方文档-</a>
+	 */
+	@GetMapping
+	@ProviderJsonApi
+	public ProviderResponseEntity<List<UserInfo>> fetchUsers(@RequestParam("user_ids") List<String> userIds) {
+		return ProviderResponseEntity.ok(this.getServiceOrThrow().fetchUsers(userIds));
+	}
 
-    private UserService userService;
+	private UserService userService;
 
-    @Autowired(required = false)
-    private void setUserService(UserService userService) {
-        this.userService = userService;
-    }
+	@Autowired(required = false)
+	private void setUserService(UserService userService) {
+		this.userService = userService;
+	}
 
-    private UserService getServiceOrThrow() {
-        if (Objects.isNull(this.userService)) {
-            String msg = String.format("request path %s not implement with interface class %s ", getRequestPath(), "UserService");
-            throw new NotImplementException(msg);
-        }
-        return this.userService;
-    }
+	private UserService getServiceOrThrow() {
+		if (Objects.isNull(this.userService)) {
+			String msg = String.format("request path %s not implement with interface class %s ", getRequestPath(),
+					"UserService");
+			throw new NotImplementException(msg);
+		}
+		return this.userService;
+	}
 
 }
